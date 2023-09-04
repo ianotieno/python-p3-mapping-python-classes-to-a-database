163b815d843f436267400622923ee1fb60ab0d51
ian otieno <ianotieno23@gmail.com>
2023-09-04 23:19:05 +0300

    third test

--- lib/song.py
+++ lib/song.py
@@ -25,3 +25,10 @@ class Song:
         """
 
         CURSOR.execute(sql, (self.name, self.album))
+
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0] 
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song    
